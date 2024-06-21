e3a976e7b566deb567be3252531e6cd07922cfc5
Reetu M Tambre <125602092+ReetuMT@users.noreply.github.com>
2024-06-21 14:07:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 
+import Address from './Compponents/Address';
 import Comments from './Compponents/Comments'
 import GetCard from './Compponents/GetCard'
 import Navbar from './Compponents/Navbar'
 import Photos from './Compponents/Photos'
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 
 function App() {
-  
   return (
     <>
-      <Navbar />
-      <GetCard /> 
-      {/* <Comments /> */}
-      {/* <Photos /> */}
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path='/users' Component={GetCard} />
+      </Routes>
+      <Routes>
+        <Route path='/comments' Component={Comments} />
+      </Routes>
+      <Routes>
+        <Route path='/photos' Component={Photos} />
+      </Routes>
+      <Routes>
+        <Route path='/address/:id' Component={Address} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
